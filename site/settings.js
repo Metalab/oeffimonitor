a870ab7d8ad791d5383468fdba9d9b90e2fd5837
burnoutberni <bernhard.hayden@gmail.com>
2015-10-04 20:48:54 +0200

    change gitignore to include settings.js again

---
+++ site/settings.js
@@ -0,0 +1,12 @@
+var api_url = 'server/index.php';
+//var api_url = 'test/response.json'; // local copy for testing
+
+var walkTimes = {
+// walkTime = walking distance in seconds
+// unreachTime = threshold to hide unreachable connections
+	"Rathaus": {"walkTime": 120, "unreachTime": 30},
+	"Schottentor": {"walkTime": 480, "unreachTime": 300},
+	"Volkstheater": {"walkTime": 480, "unreachTime": 300},
+	"Landesgerichtsstraße": {"walkTime": 480, "unreachTime": 300},
+	"Auerspergstraße": {"walkTime": 300, "unreachTime": 180}
+};
