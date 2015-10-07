0bbcefee33ee578dbd3af457cd6217450a613023
Moritz Wilhelmy <moritz+git@wzff.de>
2015-10-07 14:47:36 +0200

    minor httpd fixes

--- server/httpd.js
+++ server/httpd.js
@@ -3,23 +3,25 @@ var http = require('http');
 var settings = require(__dirname + '/settings.js');
 
 var contentTypes = {
-	'html' : 'text/html',
+	'html' : 'text/html;charset=UTF-8',
 	'js'   : 'application/javascript',
-	'json' : 'application/json',
+	'json' : 'application/json;charset=UTF-8',
 	'svg'  : 'image/svg+xml',
 	'ico'  : 'image/vnd.microsoft.icon',
-	'txt'  : 'text/plain',
+	'txt'  : 'text/plain;charset=UTF-8',
 	'css'  : 'text/css',
 };
 
 var arc = { //api response cache
 	pending          : new Set(), // set of pending response objects
 	lastUpdate       : 0,  // Date.now() of the last completed update
-	buf              : [], // the data being cached (concatenated when sent)
 	updating         : false, // currently running an update()?
-	bufferedResponse : undefined,
-	contentType      : undefined,
+	bufferedResponse : null, // the concatenated response
+	contentType      : null, // the content-type that was sent by the server
+	data             : null, // the data being cached (concatenated when sent)
 
+	// deliver a cached response or add the response object to the set of
+	// pending response handles and trigger an API update
 	add     : function (response) {
 		// cached API response not yet expired? Deliver it right away.
 		if (Date.now() - arc.lastUpdate < settings.api_cache_msec) {
@@ -31,48 +33,52 @@ var arc = { //api response cache
 		return false;
 	},
 
+	// flush all pending response objects with the buffered response
 	flush : function()
 	{
 		arc.pending.forEach(arc.deliver);
 	},
 
+	// remove a response from the list of pending response handles
 	remove  : function (response) {
 		arc.pending.delete(response);
 	},
 
+	// deliver a response from the cache and remove the response handle
+	// from pending if applicable
 	deliver : function (response) {
 		sendResponse(response, arc.bufferedResponse, arc.contentType, 200);
 		arc.remove(response);
 	},
 
-	/* send an API request, queue data in arc.data */
+	// send an API request, queue data in arc.data
 	update  : function ()
 	{
 		// update already in progress?
 		if (arc.updating)
 			return;
 		arc.updating = true;
-
 		arc.data = [];
+
 		var request = http.get(settings.api_url, function(response) {
 			response.on('data', function(chunk) {
 				if (response.statusCode !== 200)
-					response.emit('end');
+					return response.emit('error');
 				arc.contentType = response.headers['content-type'];
 				arc.data.push(chunk);
 			}).on('end', function() {
 				arc.bufferedResponse = Buffer.concat(arc.data);
 				arc.flush();
-				arc.data = undefined;
+				arc.data = null;
 				arc.lastUpdate = Date.now();
 				arc.updating = false;
 			}).on('error', function(e) {
-				console.log('API: update failed: '+e);
+				console.log('API: update failed: ' + e);
 				arc.updating = false;
 			});
 
 		}).on('error', function(e) {
-			console.log('API: update failed: '+e);
+			console.log('API: update failed: ' + e);
 			arc.updating = false;
 		});
 	},
@@ -80,9 +86,6 @@ var arc = { //api response cache
 
 function sendResponse(response, body, contentType, statusCode)
 {
-	if (body === undefined || statusCode === undefined || contentType === undefined) {
-		sendError(response, 500);
-	}
 	response.writeHead(statusCode, {
 		'Content-Length': body.length,
 		'Content-Type': contentType
@@ -92,19 +95,23 @@ function sendResponse(response, body, contentType, statusCode)
 
 function sendError(response, code)
 {
-	var err = http.STATUS_CODES[code];
-	sendResponse(response, '<h1>'+code+' '+err+'</h1>', 'text/html', code);
+	var m = code + ' ' + http.STATUS_CODES[code];
+	var html = '<!DOCTYPE html>\n<html>\n<head>\n<title>' + m +
+		'</title>\n<body>\n<h1>' + m + '</h1>\n</body>\n</html>\n';
+	sendResponse(response, html, 'text/html', code);
 }
 
 function tryStaticFile(response, path)
 {
 	try {
-		var extension = path.split('.').slice(-1)[0];
-		sendResponse(response, fs.readFileSync(__dirname+'/../site/'+path),
-		             contentTypes[extension], 200);
-		console.log("Hit: "+path);
+		var extension = path.split('.').pop();
+		var ct = contentTypes[extension];
+		ct = ct !== undefined ? ct : 'application/octet-stream';
+		var buf = fs.readFileSync(__dirname + '/../site/' + path);
+		sendResponse(response, buf, ct, 200);
+		console.log("Hit: " + path);
 	} catch (e) {
-		console.log("Miss: "+path+": "+e);
+		console.log("Miss: " + path + ": " + e);
 		sendError(response, 404);
 	}
 }
@@ -121,13 +128,13 @@ function handleRequest(request, response)
 
 	if (path === "/api") {
 		var r = arc.add(response) ? ' (cached)' : '';
-		console.log("Hit: "+path+r);
+		console.log("Hit: " + path + r);
 
 	// deadsimple directory traversal prevention
 	} else if (path.indexOf('..') === -1) {
 		tryStaticFile(response, path);
 	} else {
-		console.log("Miss: "+path+' .. in filename prohibited');
+		console.log("Miss: " + path + " (possible directory traversal)");
 		sendError(response, 403);
 	}
 }
