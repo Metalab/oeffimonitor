8bd3f3bf8efea74360610f4c4bbde8d6178dd539
Malte Zacharias <mz-basis+github@asenwelt.de>
2016-06-03 03:17:35 +0200

    Fix v0.12.0 compatibility issues, remove ES6 class ... extends ... syntax

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const debug = require('debug')('server:api');
-const settings = require("../settings");
-const Arc = require('../lib/arc.js');
+var http = require('http');
+var debug = require('debug')('server:api');
+var settings = require("../settings");
+var Arc = require('../lib/arc.js');
 var arc = new Arc({ apiUrl: settings.api_urls.realtime });
 
 module.exports = function(app, route) {
