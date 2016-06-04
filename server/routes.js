b2ae99dd58086fedb0f5777cce7f952b631f8d4c
Malte Zacharias <mz-basis+github@asenwelt.de>
2016-06-04 04:58:07 +0200

    Added api response caches for weather & forecast data

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,6 @@
 module.exports = {
   '/': require('./routes/index'),
-  '/api': require('./routes/api')
+  '/api': require('./routes/api'),
+  '/weather': require('./routes/weather'),
+  '/forecast': require('./routes/forecast')
 }
---
+++ server/routes/forecast.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var debug = require('debug')('server:api');
+var settings = require("../settings");
+var Arc = require('../lib/arc.js');
+var arc = new Arc({ apiUrl: settings.api_urls.forecast });
+
+module.exports = function(app, route) {
+  app.get(route, function (req, res, next) {
+		arc.add(res);
+  });
+
+  // Return middleware
+  return function(req, res, next) {
+    next();
+  };
+}
