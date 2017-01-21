ee57e188ab8d38b5b969b17e10fa2f0de9a5bad1
burnoutberni <hello@nini.su>
2017-01-21 19:57:59 +0100

    rewrite with express

--- server/httpd.js
+++ server/httpd.js
@@ -1,173 +1,104 @@
-var fs = require('fs');
-var http = require('http');
-var settings = require(__dirname + '/settings.js');
+const express = require('express')
+const http = require('http')
+const apicache = require('apicache')
+const settings = require(__dirname + '/settings.js');
 
-var contentTypes = {
-	'html' : 'text/html;charset=UTF-8',
-	'js'   : 'application/javascript',
-	'json' : 'application/json;charset=UTF-8',
-	'svg'  : 'image/svg+xml',
-	'ico'  : 'image/vnd.microsoft.icon',
-	'txt'  : 'text/plain;charset=UTF-8',
-	'css'  : 'text/css',
-};
+let app = express()
+let cache = apicache.middleware
+let walkcache = []
 
-var arc = { //api response cache
-	pending          : new Set(), // set of pending response objects
-	lastUpdate       : 0,  // Date.now() of the last completed update
-	updating         : false, // currently running an update()?
-	bufferedResponse : null, // the concatenated response
-	contentType      : null, // the content-type that was sent by the server
-	data             : null, // the data being cached (concatenated when sent)
+app.use(express.static('site'));
 
-	// deliver a cached response or add the response object to the set of
-	// pending response handles and trigger an API update
-	add     : function (response) {
-		// cached API response not yet expired? Deliver it right away.
-		if (Date.now() - arc.lastUpdate < settings.api_cache_msec) {
-			arc.deliver(response);
-			return true;
-		}
-		arc.pending.add(response);
-		arc.update();
-		return false;
-	},
+app.get('/api', cache(settings.api_cache_msec), (req, res) => {
+	console.log('API: new request')
+	getData((data) => res.json(data));
+})
 
-	// flush all pending response objects with the buffered response
-	flush : function()
-	{
-		arc.pending.forEach(arc.deliver);
-	},
+app.listen(settings.listen_port, () => {
+  console.log('Server up on: ', settings.listen_port);
+});
 
-	// remove a response from the list of pending response handles
-	remove  : function (response) {
-		arc.pending.delete(response);
-	},
-
-	// deliver a response from the cache and remove the response handle
-	// from pending if applicable
-	deliver : function (response) {
-		sendResponse(response, arc.bufferedResponse, arc.contentType, 200);
-		arc.remove(response);
-	},
-
-	// send an API request, queue data in arc.data
-	update  : function ()
-	{
-		// update already in progress?
-		if (arc.updating)
-			return;
-		arc.updating = true;
-		arc.data = [];
-
-		var request = http.get(settings.api_url, function(response) {
-			response.on('data', function(chunk) {
-				if (response.statusCode !== 200)
-					return response.emit('error');
-				arc.contentType = response.headers['content-type'];
-				arc.data.push(chunk);
-			}).on('end', function() {
-				arc.bufferedResponse = Buffer.concat(arc.data);
-				arc.decrapify();
-				arc.flush();
-				arc.data = null;
-				arc.lastUpdate = Date.now();
-				arc.updating = false;
-			}).on('error', function(e) {
-				console.log('API: update failed: ' + e);
-				arc.updating = false;
-			});
-
-		}).on('error', function(e) {
-			console.log('API: update failed: ' + e);
-			arc.updating = false;
+const getData = (cb) => {
+	return http.get(settings.api_url, (response) => {
+		let data = '';
+		response.on('data', (chunk) => data += chunk);
+		response.on('end', () => {
+			const json = JSON.parse(data);
+			flatten(json, cb);
 		});
-	},
+		response.on('error', (err) => console.log(err));
+	}).on('error', (err) => console.log(err));
+}
 
-	decrapify : function ()
-	{
-		let data = [];
-		JSON.parse(arc.bufferedResponse)
-			.data
-			.monitors
-			.map(monitor => {
-				monitor.lines.map(line => {
-					line.departures.departure.map(departure => {
-						data.push({
-							'stop': monitor.locationStop.properties.title,
-							'line': line.name,
-							'towards': departure.vehicle ? departure.vehicle.towards : line.towards,
-							'barrierFree': line.barrierFree,
-							'timePlanned': departure.departureTime.timePlanned,
-							'timeReal': departure.departureTime.timeReal ? departure.departureTime.timeReal : departure.departureTime.timePlanned,
-							'countdown': departure.departureTime.countdown,
-						});
-					})
-				})
+const getOSRM	= (coordinates) => {
+	const findCoordinates = (element) => {
+		return element.coordinates[0] === coordinates[0] &&
+			element.coordinates[1] === coordinates[1];
+	}
+
+	if (walkcache.find(findCoordinates)) {
+		console.log('OSRM: cached')
+		return walkcache.find(findCoordinates).duration
+	}
+
+	console.log('OSRM: new request for ' +
+		coordinates[0] + ',' +
+		coordinates[1])
+	const url = settings.osrm_api_url +
+		coordinates[0] + ',' +
+		coordinates[1] + '?overview=false';
+
+	let duration = 0;
+	const request = http.get(url, (response) => {
+		let data = '';
+		response.on('data', (chunk) => data += chunk);
+		response.on('end', () => {
+			duration = JSON.parse(data).routes[0].duration;
+			if (!walkcache.find(findCoordinates)) {
+				walkcache.push({ coordinates: coordinates, duration: duration })
+				console.log({ coordinates: coordinates, duration: duration })
+				console.log(walkcache);
+			}
+		});
+		response.on('error', (err) => console.log(err));
+	}).on('error', (err) => console.log(err));
+
+	return duration;
+}
+
+const flatten = (json, cb) => {
+	let data = [];
+	json.data.monitors.map((monitor, i) => {
+		monitor.lines.map(line => {
+			line.departures.departure.map(departure => {
+				let walkDuration = getOSRM(monitor.locationStop.geometry.coordinates);
+				let walkStatus = '';
+				if (walkDuration * 0.7 > departure.departureTime.countdown * 60) {
+					walkStatus = 'too late';
+				} else if (walkDuration > departure.departureTime.countdown * 60) {
+					walkStatus = 'hurry';
+				} else if (walkDuration + 3 * 60 > departure.departureTime.countdown * 60) {
+					walkStatus = 'soon';
+				}
+				data.push({
+					'stop': monitor.locationStop.properties.title,
+					'coordinates': monitor.locationStop.geometry.coordinates,
+					'line': line.name,
+					'towards': departure.vehicle ? departure.vehicle.towards : line.towards,
+					'barrierFree': line.barrierFree,
+					'timePlanned': departure.departureTime.timePlanned,
+					'timeReal': departure.departureTime.timeReal ? departure.departureTime.timeReal : departure.departureTime.timePlanned,
+					'countdown': departure.departureTime.countdown,
+					'walkDuration': walkDuration,
+					'walkStatus': walkStatus
+				});
 			})
-
-		data.sort((a, b) => {
-			return (a.timeReal < b.timeReal) ? -1 : ((a.timeReal > b.timeReal) ? 1 : 0);
 		})
-		arc.bufferedResponse = JSON.stringify(data);
-	},
-};
+	})
 
-function sendResponse(response, body, contentType, statusCode)
-{
-	response.writeHead(statusCode, {
-		'Content-Length': Buffer.byteLength(body, 'utf8'),
-		'Content-Type': contentType
-	});
-	response.end(body);
+	data.sort((a, b) => {
+		return (a.timeReal < b.timeReal) ? -1 : ((a.timeReal > b.timeReal) ? 1 : 0);
+	})
+	console.log(walkcache);
+	cb(data);
 }
-
-function sendError(response, code)
-{
-	var m = code + ' ' + http.STATUS_CODES[code];
-	var html = '<!DOCTYPE html>\n<html>\n<head>\n<title>' + m +
-		'</title>\n</head>\n<body>\n<h1>' + m + '</h1>\n</body>\n</html>\n';
-	sendResponse(response, html, 'text/html', code);
-}
-
-function tryStaticFile(response, path)
-{
-	try {
-		var extension = path.split('.').pop();
-		var ct = contentTypes[extension];
-		ct = ct !== undefined ? ct : 'application/octet-stream';
-		var buf = fs.readFileSync(__dirname + '/../site/' + path);
-		sendResponse(response, buf, ct, 200);
-		console.log("Hit: " + path);
-	} catch (e) {
-		console.log("Miss: " + path + ": " + e);
-		sendError(response, 404);
-	}
-}
-
-function handleRequest(request, response)
-{
-	var path = request.url.split('?')[0];
-
-	if (path === "/favicon.ico" || path === "/robots.txt")
-		return sendError(response, 404); // don't log these
-
-	if (path[path.length-1] === "/")
-		path += "index.html";
-
-	if (path === "/api") {
-		var r = arc.add(response) ? ' (cached)' : '';
-		console.log("Hit: " + path + r);
-
-	// deadsimple directory traversal prevention
-	} else if (path.indexOf('..') === -1) {
-		tryStaticFile(response, path);
-	} else {
-		console.log("Miss: " + path + " (possible directory traversal)");
-		sendError(response, 403);
-	}
-}
-
-var server = http.createServer(handleRequest);
-server.listen(settings.listen_port);
-console.log("Server started at port "+settings.listen_port);
