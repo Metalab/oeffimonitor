80921f60c7349eab40953e8cbea5973dd3ba2e6b
burnoutberni <hello@nini.su>
2017-03-04 19:13:42 +0100

    updated lines at metalab

--- server/settings.example.js
+++ server/settings.example.js
@@ -1,19 +1,19 @@
 const api_key = 'XXXXXXXXXX';
 const api_ids = [
-  "252","269", // Rathaus 2er
+  "252","269", // Rathaus 2
   "4205","4210", // Rathaus U2
-  "1346", // Landesgerichtsstraße 43er/44er & N43 (nur stadtauswärts)
-  "18", // Schottentor 1er & N25
-  "1212", // Schottentor 37
-  "1303", // Schottentor 40A
-  "3701", // Schottentor N36
-  "5568", // Schottentor N41
-  "17", // Burgtheater/Rathausplatz N60, N66
-  "1401", // Volkstheater 48A
-  "1440", // Volkstheater 49er (nur stadtauswärts)
+  "1346", // Landesgerichtsstraße 43, 44, N43 (stadtauswärts)
+  "1212", // Schottentor 37, 38, 40, 41, 42 (stadtauswärts)
+  "1303", // Schottentor 40A (stadtauswärts)
+  "3701", // Schottentor N38 (stadtauswärts, nur am Wochenende)
+  "5568", // Schottentor N41 (stadtauswärts)
+  "17", // Burgtheater/Rathausplatz D, 1, 71, N25, N38, N60, N66 (Richtung Schottentor, Nachtbusse nur wochentags)
+  "48", // Stadiongasse/Parlament D, 1, 71 (Richtung Volkstheater)
+  "1401", // Volkstheater 48A (stadtauswärts)
+  "1440", // Volkstheater 49 (stadtauswärts)
   "4909","4908", // Volkstheater U3
-  "1376", // Auerspergstraße 46er (nur stadtauswärts)
-  "5691" // Auerspergstraße N46
+  "1376", // Auerspergstraße 46 (stadtauswärts)
+  "5691", // Auerspergstraße N46 (stadtauswärts)
 ];
 
 const api_url = 'http://www.wienerlinien.at/ogd_realtime/monitor' +
